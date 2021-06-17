a29dc830567e6393f5fb3a5900e03a69aa3e0814
Luis Martinez <malbertoa_11@hotmail.com>
2021-06-17 16:37:49 -0400

    add sudo

--- entrypoint.sh
+++ entrypoint.sh
@@ -64,7 +64,7 @@ function installPipRequirements(){
 
 function runCdk(){
 	echo "Run cdk ${INPUT_CDK_SUBCOMMAND} ${*} \"${INPUT_CDK_STACK}\""
-	output=$(cdk ${INPUT_CDK_SUBCOMMAND} ${*} "${INPUT_CDK_STACK}" 2>&1)
+	output=$(sudo cdk ${INPUT_CDK_SUBCOMMAND} ${*} "${INPUT_CDK_STACK}" 2>&1)
 	exitCode=${?}
 	echo ::set-output name=status_code::${exitCode}
 	echo "${output}"
